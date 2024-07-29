e0dc160ab24615c07d4ffb0e4e35e3ea94dca8c4
Oz-Lin <lin@ozlin.info>
2024-07-29 15:25:52 +1000

    sample mnist code base

---
+++ src/ocr_handwriting_tensorflow.py
@@ -0,0 +1,54 @@
+import cv2
+import numpy as np
+import tensorflow as tf
+
+from tensorflow.keras import Sequential
+from tensorflow.keras.layers import Dense, Flatten, Conv2D, MaxPooling2D
+import matplotlib.pyplot as plt
+
+mnist = tf.keras.datasets.mnist
+#import mnist
+
+# Load the dataset
+(X_train, y_train), (X_test, y_test) = mnist.load_data()
+
+# Normalize the data
+X_train = X_train / 255.0
+X_test = X_test / 255.0
+
+# Reshape the data to add a channel dimension
+X_train = X_train.reshape(-1, 28, 28, 1)
+X_test = X_test.reshape(-1, 28, 28, 1)
+
+model = Sequential([
+    Conv2D(32, (3, 3), activation='relu', input_shape=(28, 28, 1)),
+    MaxPooling2D((2, 2)),
+    Conv2D(64, (3, 3), activation='relu'),
+    MaxPooling2D((2, 2)),
+    Flatten(),
+    Dense(128, activation='relu'),
+    Dense(10, activation='softmax')
+])
+
+model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+model.summary()
+
+model.fit(X_train, y_train, epochs=5, validation_split=0.2)
+
+loss, accuracy = model.evaluate(X_test, y_test)
+print(f'Accuracy: {accuracy}')
+
+# Load an image (example image path: 'digit.png')
+image = cv2.imread('digit.png', cv2.IMREAD_GRAYSCALE)
+image = cv2.resize(image, (28, 28))
+image = image / 255.0
+image = image.reshape(-1, 28, 28, 1)
+
+# Make a prediction
+prediction = model.predict(image)
+predicted_class = np.argmax(prediction, axis=1)
+print(f'Predicted Class: {predicted_class[0]}')
+
+plt.imshow(image.reshape(28, 28), cmap='gray')
+plt.title(f'Predicted Class: {predicted_class[0]}')
+plt.show()
